59592583db0ed826713c4cbca5fbcf3636718bed
Cory Perkins <link2cory@gmail.com>
2020-07-03 21:46:03 -0700

    Navigation in React Native Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import { View, Platform } from "react-native";
-import { createStackNavigator } from "react-navigation";
+import { createStackNavigator, createDrawerNavigator } from "react-navigation";
 
 const DirectoryNavigator = createStackNavigator(
   {
@@ -19,6 +20,27 @@ const DirectoryNavigator = createStackNavigator(
   }
 );
 
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    navigationOptions: {
+      headerStyle: { backgroundColor: "#5637DD" },
+      headerTintColor: "#fff",
+      headerTitleStyle: { color: "#fff" },
+    },
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: { screen: HomeNavigator },
+    Directory: { screen: DirectoryNavigator },
+  },
+  { drawerBackgroundColor: "#CEC8FF" }
+);
+
 class Main extends Component {
   render() {
     return (
@@ -29,7 +51,7 @@ class Main extends Component {
             Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
         }}
       >
-        <DirectoryNavigator />
+        <MainNavigator />
       </View>
     );
   }
