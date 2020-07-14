636f0eef6648cab0ea23c95e8adfb13dcd562307
Cory Perkins <link2cory@gmail.com>
2020-07-14 14:14:50 -0700

    Gestures: Gestures Part 2

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -61,10 +61,18 @@ function RenderComments({ comments }) {
 }
 
 function RenderCampsite({ campsite, favorite, markFavorite, onShowModal }) {
+  const view = React.createRef();
   const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      view.current
+        .rubberBand(1000)
+        .then((endState) =>
+          console.log(endState.finished ? "finished" : "canceled")
+        );
+    },
     onPanResponderEnd: (e, gestureState) => {
       console.log("pan responder end", gestureState);
       if (recognizeDrag(gestureState)) {
@@ -97,6 +105,7 @@ function RenderCampsite({ campsite, favorite, markFavorite, onShowModal }) {
         animation="fadeInDown"
         duration={2000}
         delay={1000}
+        ref={view}
         {...panResponder.panHandlers}
       >
         <Card
