7fdd61379056d0885b4db5921f120663315bf66c
Cory Perkins <link2cory@gmail.com>
2020-07-18 10:08:08 -0700

    Week 3 Workshop Assignment: Task 2

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -7,7 +7,7 @@ import {
   Picker,
   Switch,
   Button,
-  Modal,
+  Alert,
 } from "react-native";
 import DatePicker from "react-native-datepicker";
 import * as Animatable from "react-native-animatable";
@@ -20,17 +20,12 @@ class Reservation extends Component {
       campers: 1,
       hikeIn: false,
       date: "",
-      showModal: false,
     };
   }
 
   static navigationOptions = {
     title: "Reserve Campsite",
   };
-
-  toggleModal() {
-    this.setState({ showModal: !this.state.showModal });
-  }
 
   resetForm() {
     this.setState({
@@ -43,7 +38,28 @@ class Reservation extends Component {
 
   handleReservation() {
     console.log(JSON.stringify(this.state));
-    this.toggleModal();
+    const body =
+      `Number of Campers: ${this.state.campers}\n\n` +
+      `Hike-In? ${this.state.hikeIn}\n\n` +
+      `Date: ${this.state.date}`;
+
+    Alert.alert(
+      "Begin Search?",
+      body,
+      [
+        {
+          text: "Cancel",
+          onPress: () => this.resetForm(),
+          style: "cancel",
+        },
+        {
+          text: "OK",
+          onPress: () => this.resetForm(),
+          style: "cancel",
+        },
+      ],
+      { cancelable: false }
+    );
   }
 
   render() {
@@ -106,31 +122,6 @@ class Reservation extends Component {
             accessibilityLabel="Tap me to search for available campsites to reserve"
           />
         </View>
-        <Modal
-          animationType={"slide"}
-          transparent={false}
-          visible={this.state.showModal}
-          onRequestClose={() => this.toggleModal()}
-        >
-          <View style={styles.modal}>
-            <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-            <Text style={styles.modalText}>
-              Number of Campers: {this.state.campers}
-            </Text>
-            <Text style={styles.modalText}>
-              Hike-In?: {this.state.hikeIn ? "Yes" : "No"}
-            </Text>
-            <Text style={styles.modalText}>Date: {this.state.date}</Text>
-            <Button
-              onPress={() => {
-                this.toggleModal();
-                this.resetForm();
-              }}
-              color="#5673DD"
-              title="Close"
-            />
-          </View>
-        </Modal>
       </Animatable.View>
     );
   }
@@ -150,22 +141,6 @@ const styles = StyleSheet.create({
   },
   formItem: {
     flex: 1,
-  },
-  modal: {
-    justifyContent: "center",
-    margin: 20,
-  },
-  modalTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    backgroundColor: "#5673DD",
-    textAlign: "center",
-    color: "#fff",
-    marginBottom: 20,
-  },
-  modalText: {
-    fontSize: 18,
-    margin: 10,
   },
 });
 
